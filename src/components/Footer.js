ecab3e0b52634cd447011e0fd2e4fd5305235b7d
Admin <admin@Admins-iMac.lan>
2020-12-08 18:02:57 -0500

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/buttlesonthebus/">@buttlesonthebus</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://wanderdevelopment.com">Wander</a>.
         </span>
       </div>
     </footer>
