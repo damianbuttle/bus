53946a2e038067616d7034947dbea28f0168e0b4
Admin <admin@Admins-iMac.lan>
2020-12-08 20:26:05 -0500

    footer structue change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,17 @@ import './Footer.css'
 
 export default () => (
   <div>
+    <footer className="footer">
     <h2 className="taCenter">
       Follow us{' '}
       </h2>
-      <div className="">
+      <div className="taCenter">
       <a href="https://instagram.com/buttlesonthebus/"><i class="fab fa-instagram fa-4x"></i></a>
       <a href="https://facebook.com/buttlesonthebus/"><i class="fab fa-facebook fa-4x"></i></a>
       <a href="https://open.spotify.com/show/2wNH6OtcXWCra8NRdSLWTk?si=d2S1ey2ORfCIEnfwFFPFMQ"><i class="fab fa-spotify fa-4x"></i></a>
 
       </div>
     <br />
-    <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
